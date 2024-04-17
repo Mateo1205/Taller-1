368c0f6cf87939d89429d8d1b86b32f7970eb7a4
Mateo Cante Garzón <123739687+Mateo1205@users.noreply.github.com>
2024-04-17 00:54:12 -0500

    Add files via upload

---
+++ Taller #1/scripts/Serie.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Serie = void 0;
+class Serie {
+    constructor(id, title, network, seasons, description, website, imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.network = network;
+        this.seasons = seasons;
+        this.description = description;
+        this.website = website;
+        this.imageUrl = imageUrl;
+    }
+}
+exports.Serie = Serie;
